3468f9156152676647d9b1154b7df92689cac429
Matthew Powers <matthewkevinpowers@gmail.com>
2013-01-20 15:30:18 -0500

    added each method.  added spec directory, but cannot figure out how to make RSpec work

--- array.rb
+++ array.rb
@@ -1,4 +1,8 @@
 class Array
+	def monkey_patched?
+		true
+	end
+
 	def any?(&block)
 		self.each do |element|
 			return true if yield element
@@ -12,4 +16,22 @@ class Array
 		end
 		true
 	end
+
+	def each(&block)
+		counter = 0
+		while counter < self.length
+			yield self[counter]
+			counter += 1
+		end
+	end
 end
+
+a1 = [1, 2, 3, 4]
+p a1.monkey_patched? == true
+
+p a1.any?{ |x| x == 50 } == false
+
+a2 = []
+a1.each{ |x| a2.push(x * 2) }
+p a2 == [2, 4, 6, 8]
+
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,7 +1,17 @@
+require_relative "../array.rb"
+
 describe Array do
-	let a1 { [1, 2, 3, 4] }
+	let (:a1) { Array.new([1, 2, 3, 4]) }
+
+	context "#monkey_patched?" do
+		it "uses the monkey_patched version of the Array class" do
+			a1.monkey_patched?.should be_true
+		end
+	end
 
 	context "#any?" do
-		
+		it "1 is in [1, 2, 3, 4]" do
+			a1.any?{ |x| x == 1 }.should be_true
+		end
 	end
 end
